5722166c94334dbf7a1deeaeb5d0e3cf6403fe48
ngocnt <tanngocn@gmail.com>
2019-09-25 09:47:00 +0700

    demo-2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-
-     "parserOptions": { "ecmaVersion": 6 },
-    "env": {
-        "browser": true,
-        "es6": true
+    parser: 'babel-eslint',
+    env: {
+      browser: true,
+      es6: true,
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+    extends: [
+      'airbnb',
     ],
-    "rules": {
-        "semi": "error"
-    }
-};
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+    ],
+    rules: {
+      'default-case': 0
+    },
+  };
